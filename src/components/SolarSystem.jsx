48eb7886edc0722b47f11d1fa13112bbea609c5b
Lucas Vieira <ls6182315@gmail.com>
2023-01-05 01:19:55 +0000

    fix header error

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -6,7 +6,6 @@ import planets from '../data/planets';
 function SolarSystem() {
   return (
     <div data-testid="solar-system">
-      <h1>Sistema Solar</h1>
       <Title headline="Planetas" />
       {planets
         .map((e) => (<PlanetCard
