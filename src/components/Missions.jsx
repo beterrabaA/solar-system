b51e63c26f059216feb27e3e56f751c383a627f7
Lucas Vieira <ls6182315@gmail.com>
2023-01-05 01:20:07 +0000

    import missionCard

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import Title from './Title';
+import MissionCard from './MissionCard';
+import missions from '../data/missions';
 
 function Missions() {
   return (
-    <div data-testid="missions"><Title headline="Missões" /></div>
+    <div data-testid="missions">
+      <Title headline="Missões" />
+      {missions
+        .map((e) => (<MissionCard
+          name={ e.name }
+          year={ e.year }
+          country={ e.country }
+          destination={ e.destination }
+          key={ e.name }
+        />))}
+    </div>
   );
 }
 
