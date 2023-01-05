52328fd473672489845ac4bcd81aad181a1733d1
Lucas Vieira <ls6182315@gmail.com>
2023-01-05 01:07:16 +0000

    redering planentCard on SolarSystem component

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import planets from '../data/planets';
 
 function SolarSystem() {
   return (
     <div data-testid="solar-system">
       <h1>Sistema Solar</h1>
       <Title headline="Planetas" />
+      {planets
+        .map((e) => (<PlanetCard
+          planetName={ e.name }
+          planetImage={ e.image }
+          key={ e.name }
+        />))}
     </div>
   );
 }
