ef4137cd7e3bd4f435387656897efa42faef1e6e
Lucas Vieira <ls6182315@gmail.com>
2023-01-05 01:00:48 +0000

    redering title on SolarSystem

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React from 'react';
+import Title from './Title';
 
 function SolarSystem() {
   return (
     <div data-testid="solar-system">
       <h1>Sistema Solar</h1>
+      <Title headline="Planetas" />
     </div>
   );
 }
