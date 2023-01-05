faf64490ce0c984a68420cc8020307d45500ed7b
Lucas Vieira <ls6182315@gmail.com>
2023-01-05 00:57:31 +0000

    solarSystem creation

---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SolarSystem() {
+  return (
+    <div data-testid="solar-system">
+      <h1>Sistema Solar</h1>
+    </div>
+  );
+}
+
+export default SolarSystem;
